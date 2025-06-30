05aafdd7706b11fb0b1a1f73fbdb00fef7df1cb5
Gary Chung <kuan-i.chung@vanderbilt.edu>
2025-06-30 17:00:22 -0500

    : implement CVAE and Invariant VAE

---
+++ src/adni/models/base.py
@@ -0,0 +1,75 @@
+import torch
+from torch.nn import Module, Sequential
+from torch.nn import Conv2d, ConvTranspose2d, ReLU, Flatten
+
+
+class Encoder(Module):
+    def __init__(self, latent_dim=256):
+        super().__init__()
+        self.conv = Sequential(
+            Conv2d(1, 32, 3, stride=1, padding=1),  # 128 -> 128
+            ReLU(),
+            Conv2d(32, 32, 3, stride=2, padding=1),  # 128 -> 64
+            ReLU(),
+            Conv2d(32, 64, 3, stride=1, padding=1),  # 64 -> 64
+            ReLU(),
+            Conv2d(64, 64, 3, stride=2, padding=1),  # 64 -> 32
+            ReLU(),
+            Conv2d(64, 128, 3, stride=1, padding=1),  # 32 -> 32
+            ReLU(),
+            Conv2d(128, 128, 3, stride=2, padding=1),  # 32 -> 16
+            ReLU(),
+            Conv2d(128, 256, 3, stride=1, padding=1),  # 16 -> 16
+            ReLU(),
+            Conv2d(256, 256, 3, stride=2, padding=1),  # 16 -> 8
+            ReLU(),
+            Conv2d(256, 256, 3, stride=1, padding=1),  # 8 -> 8
+            ReLU(),
+        )
+        self.mu_enc = Conv2d(256, latent_dim, 1)
+        self.logvar_enc = Conv2d(256, latent_dim, 1)
+        self.flatten = Flatten(start_dim=1)  # Flatten the output to (batch_size, latent_dim)
+        self.latent_dim = latent_dim
+
+    def reparameterize(self, mu, logvar):
+        assert mu.shape == logvar.shape, "mu and logvar must have the same shape"
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        z = self.conv(x)
+        mu = self.mu_enc(z)
+        logvar = self.logvar_enc(z)
+        return self.reparameterize(mu, logvar), self.flatten(mu), self.flatten(logvar)
+
+
+class Decoder(Module):
+    def __init__(self, latent_dim=256):
+        super().__init__()
+        self.conv = Sequential(
+            Conv2d(latent_dim, 256, 1),
+            ReLU(),
+            Conv2d(256, 256, 3, stride=1, padding=1),  # 8 -> 8
+            ReLU(),
+            ConvTranspose2d(256, 256, 3, stride=2, padding=1, output_padding=1),  # 8 -> 16
+            ReLU(),
+            Conv2d(256, 128, 3, stride=1, padding=1),  # 16 -> 16
+            ReLU(),
+            ConvTranspose2d(128, 128, 3, stride=2, padding=1, output_padding=1),  # 16 -> 32
+            ReLU(),
+            Conv2d(128, 64, 3, stride=1, padding=1),  # 32 -> 32
+            ReLU(),
+            ConvTranspose2d(64, 64, 3, stride=2, padding=1, output_padding=1),  # 32 -> 64
+            ReLU(),
+            Conv2d(64, 32, 3, stride=1, padding=1),  # 64 -> 64
+            ReLU(),
+            ConvTranspose2d(32, 32, 3, stride=2, padding=1, output_padding=1),  # 64 -> 128
+            ReLU(),
+        )
+        self.out = Conv2d(32, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, z):
+        z = self.conv(z)
+        z = self.out(z)
+        return z
---
+++ src/adni/models/vae.py
@@ -0,0 +1,48 @@
+import torch
+from torch.nn import Module
+from .base import Encoder, Decoder
+
+
+class ConditionalVAE(Module):
+
+    def __init__(self, num_classes, latent_dim=256):
+        super(ConditionalVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = Encoder(latent_dim)
+        self.decoder = Decoder(latent_dim + num_classes)
+        self.num_classes = num_classes
+
+    def forward(self, x, y):
+        # x.shape: (1, 128, 128)
+        # y.shape: (1, num_classes)
+        # z.shape: (256, 8, 8)
+        z, mu, logvar = self.encoder(x)
+
+        # make y has (num_classes, 8, 8) shape
+        y = y.view(-1, self.num_classes, 1, 1)
+        y = y.expand(-1, -1, z.shape[-2], z.shape[-1])
+
+        # concatenate z and y
+        z = torch.cat((z, y), dim=-3)  # z.shape: (num_classes + 256, 8, 8)
+
+        x_recon = self.decoder(z)
+        return x_recon, mu, logvar
+
+
+class InvariantVAE(Module):
+
+    def __init__(self, cvae, latent_dim=256):
+        super(InvariantVAE, self).__init__()
+        self.encoder1 = cvae.encoder
+        for param in self.encoder1.parameters():
+            param.requires_grad = False
+        self.encoder = Encoder(latent_dim)
+        self.decoder = Decoder(latent_dim + cvae.latent_dim)
+
+    def forward(self, x):
+        z1, _, _ = self.encoder1(x)
+        z2, mu, logvar = self.encoder(x)
+        # Concatenate z1 and z2
+        z = torch.cat((z1, z2), dim=-3)  # z.shape: (num_classes + 256, 8, 8)
+        x_recon = self.decoder(z)
+        return x_recon, mu, logvar
