428d0ab94f36d102debf27024e91d04a28699acd
Gary Chung <kuan-i.chung@vanderbilt.edu>
2025-07-02 23:45:47 -0500

    refactor: change file names of scripts

--- mnist_invarep.sh
+++ mnist_invarep.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-beta1=0.001
-beta2_values=(0.06 0.08 0.09)
+beta1=0.005
+beta2_values=(0.001 0.003 0.005 0.007 0.009 0.01 0.1 1)  #  0.007 0.009 0.01
 echo "======================================================"
 echo "run beta1 values: $beta1"
 echo "======================================================"
+
 
 echo "beta1 = $beta1 Phase 1 Training for CVAE"
 CUDA_VISIBLE_DEVICES=1 python -m src.mnist.group_trainers.invarep.main_phase1 --beta1 $beta1 --epochs 500 --batch_size 500 --lr 5e-4
