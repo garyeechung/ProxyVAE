5437efd29b3ba941819fa47253977892ebd90ab7
Gary Chung <kuan-i.chung@vanderbilt.edu>
2025-07-04 15:35:14 -0500

    refactor: add some line, preventing flake8 warning

--- src/adni/models/__init__.py
+++ src/adni/models/__init__.py
@@ -1,2 +1,9 @@
 from .vae import ConditionalVAE, InvariantVAE
 from .posthocs import ProxyRep2InvarRep, VariationalPredictor
+
+
+if __name__ == "__main__":
+    print(ConditionalVAE.__name__)
+    print(InvariantVAE.__name__)
+    print(ProxyRep2InvarRep.__name__)
+    print(VariationalPredictor.__name__)
