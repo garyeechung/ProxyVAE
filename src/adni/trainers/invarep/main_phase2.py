f8c907dd5cf8cb6093780c75d13e20d9aed2f89e
Gary Chung <kuan-i.chung@vanderbilt.edu>
2025-07-06 22:40:14 -0500

    feat: implement phase 2 training

---
+++ src/adni/trainers/invarep/main_phase2.py
@@ -0,0 +1,138 @@
+import argparse
+import os
+
+import pandas as pd
+import torch
+
+from src.adni.models import ConditionalVAE, InvariantVAE, ProxyRep2InvaRep, VariationalPredictor
+from src.adni.datasets import get_adni_dataloaders
+from .proxyvae import train_proxyvae
+from .proxy2invarep import train_proxy2invarep
+from .posthoc import train_posthoc_predictor
+
+
+def main(args):
+    df = pd.read_csv(os.path.join(args.data_dir, "adni_data.csv"))
+    dataloaders = get_adni_dataloaders(
+        df, data_dir=os.path.join(args.data_dir, "FA_rigid_MNI_1mm"),
+        targets=["manufacturer_id", "model_type_id"],
+        batch_size=args.batch_size, include_mappable_site_empty=False,
+        cache_type="persistent", bootstrap=args.bootstrap,
+        num_workers=0, batch_per_epoch=args.batch_per_epoch,
+        spatial_size=args.spatial_size,
+        slice_range_from_center=args.slice_range_from_center
+    )
+    print(f"Using device: {args.device}")
+    print(f"Batch size: {args.batch_size}")
+    print(f"train: {len(dataloaders[0].dataset)} samples")
+    print(f"valid: {len(dataloaders[1].dataset)} samples")
+    print(f"test: {len(dataloaders[2].dataset)} samples")
+    print(f"unknown: {len(dataloaders[3].dataset)} samples")
+
+    cvae_ckpt = os.path.join(args.ckpt_dir, "invarep", f"beta1_{args.beta1:.1E}", "cvae_best.pth")
+    if not os.path.exists(cvae_ckpt):
+        print(f"ConditionalVAE checkpoint not found at {cvae_ckpt}")
+        return
+
+    cvae_ckpt = torch.load(cvae_ckpt, weights_only=False)
+    cvae = ConditionalVAE(num_classes=3)
+    cvae.load_state_dict(cvae_ckpt["model_state_dict"])
+    for param in cvae.parameters():
+        param.requires_grad = False
+    torch.cuda.empty_cache()
+
+    # Second phase: Train the Invariant Variational Autoencoder (ProxyVAE)
+    proxyvae = InvariantVAE(cvae=cvae)
+    proxyvae = proxyvae.to(args.device)
+    print(f"Training ProxyVAE with beta1={args.beta1}, beta2={args.beta2}")
+    train_proxyvae(proxyvae, train_loader=dataloaders[0], valid_loader=dataloaders[1],
+                   ckpt_dir=args.ckpt_dir,
+                   x_key="image",
+                   beta1=args.beta1,
+                   beta2=args.beta2,
+                   bootstrap=args.bootstrap,
+                   device=args.device,
+                   epochs=args.epochs,
+                   lr=args.lr,
+                   if_existing_ckpt="resume")
+    torch.cuda.empty_cache()
+
+    # Freeze the parameters of ProxyVAE, train z2 -> z1 predictor
+    proxyvae = proxyvae.to("cpu")
+    for param in proxyvae.parameters():
+        param.requires_grad = False
+
+    proxy2invarep = ProxyRep2InvaRep(proxyvae, image_size=args.spatial_size)
+    proxy2invarep = proxy2invarep.to(args.device)
+    print(f"Training ProxyRep2InvaRep with beta1={args.beta1}, beta2={args.beta2}")
+    train_proxy2invarep(proxy2invarep, train_loader=dataloaders[0], valid_loader=dataloaders[1],
+                        ckpt_dir=args.ckpt_dir,
+                        x_key="image",
+                        beta1=args.beta1,
+                        beta2=args.beta2,
+                        bootstrap=args.bootstrap,
+                        device=args.device,
+                        epochs=args.epochs,
+                        lr=args.lr,
+                        if_existing_ckpt="resume")
+    proxy2invarep = proxy2invarep.to("cpu")
+    torch.cuda.empty_cache()
+
+    # Post-hoc predictor for manufacturer_id
+    posthoc_group = VariationalPredictor(encoder=proxy2invarep.encoder2,
+                                         num_classes=3, is_posthoc=True)
+    posthoc_group = posthoc_group.to(args.device)
+    print(f"Training post-hoc predictor for manufacturer_id with beta1={args.beta1}, beta2={args.beta2}")
+    train_posthoc_predictor(posthoc_group, train_loader=dataloaders[0], valid_loader=dataloaders[1],
+                            ckpt_dir=args.ckpt_dir,
+                            x_key="image", y_key="manufacturer_id",
+                            beta1=args.beta1, beta2=args.beta2, device=args.device,
+                            bootstrap=args.bootstrap, epochs=args.epochs,
+                            lr=args.lr, if_existing_ckpt="resume")
+    posthoc_group = posthoc_group.to("cpu")
+    torch.cuda.empty_cache()
+
+    # Post-hoc predictor for model_type_id
+    posthoc_class = VariationalPredictor(encoder=proxy2invarep.encoder2,
+                                         num_classes=9, is_posthoc=True)
+    posthoc_class = posthoc_class.to(args.device)
+    print(f"Training post-hoc predictor for model_type_id with beta1={args.beta1}, beta2={args.beta2}")
+    train_posthoc_predictor(posthoc_class, train_loader=dataloaders[0], valid_loader=dataloaders[1],
+                            ckpt_dir=args.ckpt_dir,
+                            x_key="image", y_key="model_type_id",
+                            beta1=args.beta1, beta2=args.beta2, device=args.device,
+                            bootstrap=args.bootstrap, epochs=args.epochs,
+                            lr=args.lr, if_existing_ckpt="resume")
+    posthoc_class = posthoc_class.to("cpu")
+    torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train ProxyVAE and post-hocs for ADNI")
+    parser.add_argument("--data_dir", type=str,
+                        default="/home/chungk1/Repositories/InvaRep/data/ADNI/",
+                        help="Directory for ADNI data")
+    parser.add_argument("--ckpt_dir", type=str,
+                        default="/home/chungk1/Repositories/InvaRep/checkpoints/adni/manufacturer",
+                        help="Directory to save checkpoints")
+    parser.add_argument("--beta1", type=float, default=1.0, help="Beta1 parameter for CVAE loss")
+    parser.add_argument("--beta2", type=float, default=1.0, help="Beta2 parameter for ProxyVAE loss")
+    parser.add_argument("--batch_size", type=int, default=128, help="Batch size for training")
+    parser.add_argument("--lr", type=float, default=5e-4, help="Learning rate for optimizer")
+    parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train")
+    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu",
+                        help="Device to use for training (cuda or cpu)")
+    parser.add_argument("--batch_per_epoch", type=int, default=100,
+                        help="Number of batches per epoch")
+    parser.add_argument("--bootstrap", action="store_true",
+                        help="Whether to bootstrap the dataset")
+    parser.add_argument("--spatial_size", type=int, nargs=2, default=[224, 224],
+                        help="Spatial size of the images")
+    parser.add_argument("--slice_range_from_center", type=float, default=0.03,
+                        help="Slice range from center for the images")
+    parser.add_argument("--if_existing_ckpt", type=str, default="resume",
+                        choices=["resume", "replace", "pass"],
+                        help="What to do if an existing checkpoint is found")
+    args = parser.parse_args()
+
+    main(args)
