7565fb4f5ba0ac24a1bd65ed4289f5e708d0c476
Gary Chung <kuan-i.chung@vanderbilt.edu>
2025-07-20 23:26:35 -0500

    refactor: prun redundant directory tree

--- scripts/adni_infobottleneck.sh
+++ scripts/adni_infobottleneck.sh
@@ -14,7 +14,7 @@ echo "======================================================"
 
 for beta in ${beta_values[@]}; do
     echo "beta = $beta info bottleneck training"
-    nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.infobottleneck.main --beta $beta --epochs $epochs --batch_size $batch_size --bootstrap --lr $lr --backbone $backbone --modality $modality --bound_z_by $bound_z_by > logs/$modality/$backbone-$bound_z_by/ib_beta_$(printf "%.1E" $beta).log 2>&1
+    nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.infobottleneck --beta $beta --epochs $epochs --batch_size $batch_size --bootstrap --lr $lr --backbone $backbone --modality $modality --bound_z_by $bound_z_by > logs/$modality/$backbone-$bound_z_by/ib_beta_$(printf "%.1E" $beta).log 2>&1
 
 done
 echo "All runs completed."
--- scripts/adni_invarep.sh
+++ scripts/adni_invarep.sh
@@ -17,11 +17,11 @@ echo "======================================================"
 
 for beta1 in ${beta1_values[@]}; do
     echo "beta1 = $beta1 Phase 1 Training"
-    nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.invarep.main_phase1 --beta1 $beta1 --epochs $epochs_phase1 --batch_size $batch_size --bootstrap --lr $lr_phase1 --backbone $backbone --bound_z_by $bound_z_by --modality $modality --batch_per_epoch $batch_per_epoch > logs/$modality/$backbone-$bound_z_by/phase1_beta1_$(printf "%.1E" $beta1).log 2>&1
+    nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.main_phase1 --beta1 $beta1 --epochs $epochs_phase1 --batch_size $batch_size --bootstrap --lr $lr_phase1 --backbone $backbone --bound_z_by $bound_z_by --modality $modality --batch_per_epoch $batch_per_epoch > logs/$modality/$backbone-$bound_z_by/phase1_beta1_$(printf "%.1E" $beta1).log 2>&1
 
     for beta2 in ${beta2_values[@]}; do
         echo "beta1 = $beta1, beta2 = $beta2 Phase 2 Training"
-        nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.invarep.main_phase2 --beta1 $beta1 --beta2 $beta2 --epochs $epochs_phase2 --batch_size $batch_size --bootstrap --lr $lr_phase2 --backbone $backbone --bound_z_by $bound_z_by --modality $modality --batch_per_epoch $batch_per_epoch > logs/$modality/$backbone-$bound_z_by/phase2_beta1_$(printf "%.1E" $beta1)_beta2_$(printf "%.1E" $beta2).log 2>&1
+        nohup env CUDA_VISIBLE_DEVICES=$device python -m src.cnn.trainers.adni.main_phase2 --beta1 $beta1 --beta2 $beta2 --epochs $epochs_phase2 --batch_size $batch_size --bootstrap --lr $lr_phase2 --backbone $backbone --bound_z_by $bound_z_by --modality $modality --batch_per_epoch $batch_per_epoch > logs/$modality/$backbone-$bound_z_by/phase2_beta1_$(printf "%.1E" $beta1)_beta2_$(printf "%.1E" $beta2).log 2>&1
     done
 
 done
